6c3e6d0e3d15e4beed6e870aecca20f2dada32ef
Catalin Barsan <bcn@catalinbarsan.es>
2024-09-25 18:41:50 +0200

    Level 3, Task 1: Fix bug in initializeData method of Cinema class; improve input handling with try-catch.

--- src/nivel03/ejercicio01/main/java/com/modules/cinema/Cinema.java
+++ src/nivel03/ejercicio01/main/java/com/modules/cinema/Cinema.java
@@ -2,6 +2,7 @@ package nivel03.ejercicio01.main.java.com.modules.cinema;
 
 import nivel03.ejercicio01.main.java.com.modules.seat.SeatManager;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Cinema {
@@ -21,11 +22,15 @@ public class Cinema {
     }
 
     public void initializeData() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\nMENU" + "\nEnter the number of rows in the cinema:");
-        totalRows = scanner.nextInt();
-        System.out.println("Enter the number of seats per row:");
-        seatsPerRow = scanner.nextInt();
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\nMENU" + "\nEnter the number of rows in the cinema:");
+            totalRows = scanner.nextInt();
+            System.out.println("Enter the number of seats per row:");
+            seatsPerRow = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("\nInvalid input. Please try again.");
+        }
     }
 
     public int getTotalRows() {
