b4807e46f6931e57727897d7de3e640bcd3dccc9
Catalin Barsan <bcn@catalinbarsan.es>
2024-10-07 16:43:04 +0200

    Level 01, Task 01 - fix add Start class to put all the logical.

---
+++ src/nivel01/ejercicio01/main/java/com/modules/sales/Start.java
@@ -0,0 +1,47 @@
+package nivel01.ejercicio01.main.java.com.modules.sales;
+
+import nivel01.ejercicio01.main.java.com.modules.products.Product;
+import nivel01.ejercicio01.main.java.com.modules.products.ProductManager;
+import nivel01.ejercicio01.main.java.com.modules.salebuida.SaleBuidaException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.out;
+
+public class Start {
+
+    public void start() throws SaleBuidaException {
+        try {
+
+            ProductManager pm = new ProductManager();
+            List<Product> productList = pm.getProducts();
+
+            Sale sale1 = new Sale(productList);
+            sale1.getTotalPrice();
+
+            try {
+                Product invalidProduct = productList.get(2);
+                out.println("Product " + invalidProduct.getName());
+            } catch (IndexOutOfBoundsException e) {
+                out.println("Error: " + e.getMessage());
+                out.println("Index out of bounds.");
+            }
+
+            List<Product> productList2 = new ArrayList<>();
+            Sale sale2 = new Sale(productList2);
+
+            try {
+                sale2.getTotalPrice();
+            } catch (SaleBuidaException e) {
+                out.println("Error: " + e.getMessage());
+            }
+
+        } catch (Exception e) {
+            out.println("Unexpected error occurred: " + e.getMessage());
+        }
+
+    }
+
+
+}
