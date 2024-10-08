a48c8350b8cf842aa00e3b722018b413fab6bdcb
Catalin Barsan <bcn@catalinbarsan.es>
2024-10-08 11:12:54 +0200

    Level 01 task 01 - add try-catch to main method.

--- src/nivel01/ejercicio01/main/java/com/modules/Main.java
+++ src/nivel01/ejercicio01/main/java/com/modules/Main.java
@@ -5,10 +5,14 @@ import nivel01.ejercicio01.main.java.com.modules.sales.Start;
 
 public class Main {
 
-    public static void main(String[] args) throws SaleBuidaException {
+    public static void main(String[] args){
 
         Start app = new Start();
-        app.start();
+        try {
+            app.start();
+        } catch (SaleBuidaException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
